5a6732a2f343862d5b4912d290a4daec2f0c1fec
Edward Coates <escoates1@gmail.com>
2026-07-14 17:27:51 +0100

    Remove unused snowflake grant for USGS Earthquake access integration

--- terraform/environments/account/main.tf
+++ terraform/environments/account/main.tf
@@ -5,16 +5,3 @@ module "role" {
     snowflake = snowflake.securityadmin
   }
 }
-
-# Lets DBT_TRANSFORMATIONS (EXECUTE AS CALLER) invoke INGEST_USGS_EARTHQUAKES,
-# which needs outbound network access via this integration.
-resource "snowflake_grant_privileges_to_account_role" "dbt_external_access_integration" {
-  provider = snowflake.securityadmin
-
-  account_role_name = module.role.dbt_runs_role
-  privileges        = ["USAGE"]
-  on_account_object {
-    object_type = "INTEGRATION"
-    object_name = "USGS_EARTHQUAKE_ACCESS_INTEGRATION"
-  }
-}
